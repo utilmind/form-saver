aea68ccca946799a21a510417ad6079b9a9b384e
Oleksii Kuznietsov <utilmind@gmail.com>
2026-05-10 14:01:43 -0400

    Update domControls.ts

--- react/src/domControls.ts
+++ react/src/domControls.ts
@@ -71,9 +71,8 @@ export const collectDomFormValues = (
 ): FormSaverValues => {
     const controls = getDomFormControls(root, options)
     const values: FormSaverValues = {}
-    const len = controls.length
 
-    for (let i = 0; i < len; ++i) {
+    for (let i = 0; i < controls.length; ++i) {
         const ctrl = controls[i]
         const name = ctrl.name
         const tag = ctrl.tagName
